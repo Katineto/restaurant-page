a731b57719ff770dc2b7fd2eb32a237c90108ccc
Katineto <not.a.mielikki@gmail.com>
2018-07-18 14:09:20 +0700

    Remove typos

--- src/home-tab.js
+++ src/home-tab.js
@@ -7,7 +7,7 @@ function loadHomeTab(){
     const tastyText = document.createElement('p')
     tastyText.innerText = `The Dessert Place have been established in 2018.
     Our team is driven by the passion of making healthy delicious sweets.
-    Check our menu for a variety of gluten-free muffins and vegan cakes, yougurt ice pops with fresh seasonal berries perfect for a hot day and many more delicious desserts.`
+    Check our menu for a variety of gluten-free muffins and vegan cakes, yoghurt ice pops with fresh seasonal berries and many more delicious desserts.`
 
     content.appendChild(tastyText)
 }
